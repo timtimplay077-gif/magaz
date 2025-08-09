a2f48af6f98a45bade837c3b26163c970b6e07b8
timtimplay077-gif <timtimplay077@gmail.com>
2025-08-10 02:27:08 +0300

    d

--- components/slider.php
+++ components/slider.php
@@ -12,6 +12,6 @@
         <img src="slider/sldier_photo_6.jpg" class="slider_img">
     </div>
        <div>
-        <img src="slider/slider_photo_7.jpg" class="slider_img">
+        <img src="slider/sldier_photo_7.jpg" class="slider_img">
     </div>
 </section>
