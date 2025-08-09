254ff74f6e9ea8d316f465e7ead636d1f295a4d9
timtimplay077-gif <timtimplay077@gmail.com>
2025-08-10 02:05:28 +0300

    s

--- components/slider.php
+++ components/slider.php
@@ -3,15 +3,15 @@
         <img src="slider/sliderphoto1.jpg" class="slider_img">
     </div>
     <div>
-        <img src="slider/slider_photo_7.jpg" class="slider_img">
+        <img src="slider/slider_photo_3.jpg" class="slider_img">
     </div>
     <div>
         <img src="slider/sldier_photo_4.jpg" class="slider_img">
     </div>
     <div>
-        <img src="slider/sldier_photo_5.jpg" class="slider_img">
-    </div>
-    <div>
         <img src="slider/sldier_photo_6.jpg" class="slider_img">
+    </div>
+       <div>
+        <img src="slider/slider_photo_7.jpg" class="slider_img">
     </div>
 </section>
