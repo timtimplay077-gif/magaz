fcc7d527868d56451264930617276d47712da97b
timtimplay077-gif <timtimplay077@gmail.com>
2025-08-12 18:08:38 +0300

    і

--- productBasket.php
+++ productBasket.php
@@ -14,9 +14,10 @@ $basket_product_query = $db_conn->query($basket_product);
 <div class="overlay" id="overlay" onclick="closeCart()"></div>
 <div class="modal modal-basket" id="cartModal">
     <div class="cart-title">Кошик</div>
-    <hr>
+
 
     <div id="cart-items">
+        <hr>
         <?php while ($row = $basket_product_query->fetch_assoc()) { ?>
             <div class="header_card_product" data-id="<?php echo $row['id']; ?>" data-price="<?php echo $row['price']; ?>">
                 <div class="delete-wrapper">
