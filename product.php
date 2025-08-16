bc2ba571cd96f36cd025b3dc695117722835ecb4
timtimplay077-gif <timtimplay077@gmail.com>
2025-08-16 12:45:44 +0300

    ssssssss

--- product.php
+++ product.php
@@ -74,66 +74,68 @@ $db_imgage_query = $db_conn->query($db_imgage_sql);
     <div class="product_row unselectable">
         <div class="block">
             <div class="slider_wrapper2">
-                <div class="arrow_l1" onclick="slider_product('left')">
-                    <i class="fa-solid fa-chevron-left"></i>
-                </div>
-                <div class="arrow_r1" onclick="slider_product('right')">
-                    <i class="fa-solid fa-chevron-right"></i>
-                </div>
-                <img class="slider_product" src="<?php print_r($row["img"]) ?>" alt="">
-                <div class="product_photo_slider">
-                    <?php
-                    $images = "";
-                    for ($i = 0; $i < $db_imgage_query->num_rows; $i++) {
-                        $db_imgage_row = $db_imgage_query->fetch_assoc();
-                        $images .= "'" . $db_imgage_row["img"] . "',";
-                        $img = $db_imgage_row["img"];
-                        ?>
-                        <img onclick="set_mimiImg('<?= $i ?>')" src="<?php print_r($img) ?>" alt="">
-                    <?php } ?>
-                </div>
-                <script>
-                    const slider_wrapper_product = [<?= $images ?>];
-                </script>
-            </div>
-            <div class="product_row_about">
-                <div class="status">
-                    <p class="stock_status">В наявності</p>
-                </div>
-                <div class="product_manufacturer">
-
-                    <p>Код: <?php print_r($row["productСode"]) ?></p>
-                </div>
-                <?php
-                $original_price = $row['price'];
-                $discount_price = $original_price;
-
-                if (isset($_SESSION['id'])) {
-                    $user_id = $_SESSION['id'];
-                    $user_sql = "SELECT sale FROM users WHERE id = '$user_id'";
-                    $user_result = $db_conn->query($user_sql);
-                    if ($user_result && $user_result->num_rows > 0) {
-                        $user_row = $user_result->fetch_assoc();
-                        if ($user_row['sale'] > 0) {
-                            $discount_price = $original_price * (1 - $user_row['sale'] / 100);
-                        }
-                    }
-                }
-                ?>
-                <div class="product_row_price">
-                    <div class="price">
-                        <?= round(num: $discount_price, precision: 2) ?> ₴
+                <?php if ($db_image_query->num_rows > 1): ?>
+                    <div class="arrow_l1" onclick="slider_product('left')">
+                        <i class="fa-solid fa-chevron-left"></i>
                     </div>
-                </div>
 
-                <div class="product_row_about_buy">
-                    <a href="addcart.php?user_id=<?= $user_id ?>&product_id=<?= $row['id'] ?>">
-                        <img src="contact/shopping-bag.png" alt="" class="product_row_about_buy">Купить
-                    </a>
-                </div>
+                    <div class="arrow_r1" onclick="slider_product('right')">
+                        <i class="fa-solid fa-chevron-right"></i>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <?php
+            $images = "";
+            for ($i = 0; $i < $db_imgage_query->num_rows; $i++) {
+                $db_imgage_row = $db_imgage_query->fetch_assoc();
+                $images .= "'" . $db_imgage_row["img"] . "',";
+                $img = $db_imgage_row["img"];
+                ?>
+                <img onclick="set_mimiImg('<?= $i ?>')" src="<?php print_r($img) ?>" alt="">
+            <?php } ?>
+        </div>
+        <script>
+            const slider_wrapper_product = [<?= $images ?>];
+        </script>
+    </div>
+    <div class="product_row_about">
+        <div class="status">
+            <p class="stock_status">В наявності</p>
+        </div>
+        <div class="product_manufacturer">
 
+            <p>Код: <?php print_r($row["productСode"]) ?></p>
+        </div>
+        <?php
+        $original_price = $row['price'];
+        $discount_price = $original_price;
+
+        if (isset($_SESSION['id'])) {
+            $user_id = $_SESSION['id'];
+            $user_sql = "SELECT sale FROM users WHERE id = '$user_id'";
+            $user_result = $db_conn->query($user_sql);
+            if ($user_result && $user_result->num_rows > 0) {
+                $user_row = $user_result->fetch_assoc();
+                if ($user_row['sale'] > 0) {
+                    $discount_price = $original_price * (1 - $user_row['sale'] / 100);
+                }
+            }
+        }
+        ?>
+        <div class="product_row_price">
+            <div class="price">
+                <?= round(num: $discount_price, precision: 2) ?> ₴
             </div>
         </div>
+
+        <div class="product_row_about_buy">
+            <a href="addcart.php?user_id=<?= $user_id ?>&product_id=<?= $row['id'] ?>">
+                <img src="contact/shopping-bag.png" alt="" class="product_row_about_buy">Купить
+            </a>
+        </div>
+
+    </div>
+    </div>
     </div>
     <div class="product_delivery_payment unselectable">
         <div class="block">
