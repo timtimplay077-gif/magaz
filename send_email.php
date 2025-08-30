0adbed57623d3d847662ab29656062db00f8c6ce
timtimplay077-gif <timtimplay077@gmail.com>
2025-08-30 17:18:25 +0300

    ффываф

--- send_email.php
+++ send_email.php
@@ -23,7 +23,7 @@ if ($order_query && $row = $order_query->fetch_assoc()) {
     die("Не вдалося отримати email одержувача");
 }
 $mail_host = "smtp.gmail.com";
-$mail_username = "kanskrop@gmail.com";
+$mail_username = "pykpykdaun69@gmail.com";
 $mail_to = "kanskrop@gmail.com";
 $firstName = $_GET['firstName'] ?? '';
 $lastName = $_GET['lastName'] ?? '';
